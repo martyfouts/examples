ce00c88d96798058656bff3f5fc84e7b3e548462
marty <fouts@fogey.com>
2022-03-05 09:25:07 -0800

    SE: move strips in the NLA

--- animationNLA.py
+++ animationNLA.py
@@ -50,3 +50,49 @@ object.rotation_euler = (0,1,0)
 object.keyframe_insert("rotation_euler", frame = 13)
 object.animation_data.action.name = "turn me"
 stash(object, object.animation_data.action, 'turn track', 1)
+
+#------------------------------------------------------------------------------
+# https://blender.stackexchange.com/questions/255423/move-strips-and-add-transition-in-nla-editor-using-python
+# Move strips between tracks in the NLA
+
+# Assume we want to deal with actions associated with the active object
+object = bpy.context.active_object
+tracks = object.animation_data.nla_tracks
+
+# For a different set up, replace "NlaTrack.002" with the name of the destination track
+dst_track = tracks["NlaTrack.002"]
+
+# For a different set up, replace "NlaTrack.001" with the name of the source track.
+src_track = tracks["NlaTrack.001"]
+
+# Assume that the strip to be moved is the first strip on the track.
+# We could, instead, simply select an action from bpy.data.actions and not bother
+# with an additional strip
+src_strip = src_track.strips[0]
+
+# We really need the action, not the strip
+action = src_strip.action
+
+# Since we are "moving" the strip, we delete it from the src track
+src_track.strips.remove(src_strip)
+
+# The 2nd argument is the frame number where the action goes. It has to be an integer.
+# We assume that it goes adjacent to the last strip already on the track.
+new_strip = dst_track.strips.new(action.name, int(dst_track.strips[-1].frame_end+1), action)
+
+# Now you can use new_strip to set transition or add modifiers.  See the manual entry
+# https://docs.blender.org/api/current/bpy.types.NlaStrips.htm for details on the fields
+# for instance
+# new_strip.blend_in = SOME_FRAME_NUMBER
+
+
+# This is unnecessary for just "moving" a strip.  Here only for completeness.
+tracks.remove(src_track)
+
+# This is just a repeat of the above, but applied to a different track.
+src_track = tracks["NlaTrack"]
+src_strip = src_track.strips[0]
+action = src_strip.action
+src_track.strips.remove(src_strip)
+new_strip = dst_track.strips.new(action.name, int(dst_track.strips[-1].frame_end+1), action)
+tracks.remove(src_track)
